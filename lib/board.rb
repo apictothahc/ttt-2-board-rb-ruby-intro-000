a441d8e2297c805b34727c5771328db2095cd9ed
Jack Bachmann <jackbachmann@me.com>
2020-11-09 02:47:08 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,6 @@
 # Define the variable board below.
-board = Array.new (row = [" ", " ", " "]
-
-row1 = [" ", " ", " "]
-
-row2 = [" ", " ", " "]
-)
+board =  [
+  " ", " ", " "
+  " ", " ", " "
+  " ", " ", " "
+]
